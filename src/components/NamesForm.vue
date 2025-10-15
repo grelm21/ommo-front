0323d2fecfbae7c4b16b4f9e8eb471822aad6b81
Grigory Mitrofanov <grelm.wolski@gmail.com>
2025-10-15 17:38:34 +0200

    added wawe backgorund

--- src/components/NamesForm.vue
+++ src/components/NamesForm.vue
@@ -7,40 +7,47 @@ const partner_one = ref('')
 const partner_two = ref('')
 
 const handleSubmit = async () => {
-  await contractStore.createContract({
-    contract: {
-      partner_one: {
-        name: partner_one.value,
-        promise_id: 1,
-      },
-      partner_two: {
-        name: partner_two.value,
-        promise_id: 2,
-      },
-    },
-  })
+  await contractStore.updatePartnerOne(partner_one.value, null)
+  await contractStore.updatePartnerTwo(partner_two.value, null)
 }
 </script>
 
 <template>
   <form
-    class="flex flex-col w-[512px] items-center bg-white rounded-lg shadow-md h-[512px]"
+    class="form-container"
     @submit.prevent="handleSubmit"
   >
-    <input
-      v-model="partner_one"
-      type="text"
-      placeholder="Партнер 1"
-      class="w-full px-4 py-2 border-b border-gray-300 focus:outline-none focus:border-sky-500"
-    />
-    <input
-      v-model="partner_two"
-      type="text"
-      placeholder="Партнер 2"
-      class="w-full px-4 py-2 border-b border-gray-300 focus:outline-none focus:border-sky-500"
-    />
-    <button type="submit">Создать договор</button>
+
+    <div class="title">ioio</div>
+    <!-- <input -->
+    <!--   v-model="partner_one" -->
+    <!--   type="text" -->
+    <!--   placeholder="Партнер 1" -->
+    <!--   class="w-full px-4 py-2 border-b border-gray-300 focus:outline-none focus:border-sky-500" -->
+    <!-- /> -->
+    <!-- <input -->
+    <!--   v-model="partner_two" -->
+    <!--   type="text" -->
+    <!--   placeholder="Партнер 2" -->
+    <!--   class="w-full px-4 py-2 border-b border-gray-300 focus:outline-none focus:border-sky-500" -->
+    <!-- /> -->
+    <!-- <button type="submit">Создать договор</button> -->
   </form>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .form-container {
+    @apply flex flex-col w-[460px] items-center rounded-lg shadow-md h-[723px];
+    position: relative;
+    background: linear-gradient(to bottom, #0B002D, #0B0014);
+  }
+
+  .title {
+    background-image: url('@/assets/waves-form.svg');
+    background-size: contain;
+    background-repeat: no-repeat;
+    width: 100%;
+    height: 100%;
+
+  }
+</style>
