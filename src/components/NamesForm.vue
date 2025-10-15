202ea49dde600f625716bd71ad7755527d968aa6
Grigory Mitrofanov <grelm.wolski@gmail.com>
2025-10-15 16:45:12 +0200

    added contract storage, name form

--- src/components/NamesForm.vue
+++ src/components/NamesForm.vue
@@ -1,18 +1,46 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import { useContractStore } from '@/stores/contractStore'
+const contractStore = useContractStore()
+
+const partner_one = ref('')
+const partner_two = ref('')
+
+const handleSubmit = async () => {
+  await contractStore.createContract({
+    contract: {
+      partner_one: {
+        name: partner_one.value,
+        promise_id: 1,
+      },
+      partner_two: {
+        name: partner_two.value,
+        promise_id: 2,
+      },
+    },
+  })
+}
+</script>
 
 <template>
-  <div class="names-field flex p-4 text-[20px] items-end gap-x-4">
-    <span>Договор между </span>
-    <div class="field">
-      <input type="text" placeholder="Партнер А" class="" />
-      <div class="line"></div>
-    </div>
-    <span> и </span>
-    <div class="field">
-      <input type="text" placeholder="Партнер Б" class="" />
-      <div class="line"></div>
-    </div>
-  </div>
+  <form
+    class="flex flex-col w-[512px] items-center bg-white rounded-lg shadow-md h-[512px]"
+    @submit.prevent="handleSubmit"
+  >
+    <input
+      v-model="partner_one"
+      type="text"
+      placeholder="Партнер 1"
+      class="w-full px-4 py-2 border-b border-gray-300 focus:outline-none focus:border-sky-500"
+    />
+    <input
+      v-model="partner_two"
+      type="text"
+      placeholder="Партнер 2"
+      class="w-full px-4 py-2 border-b border-gray-300 focus:outline-none focus:border-sky-500"
+    />
+    <button type="submit">Создать договор</button>
+  </form>
 </template>
 
 <style scoped></style>
