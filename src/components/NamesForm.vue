87a0009c5ffdcfd36cd43e610b7cdcfb80af41bc
Grigory Mitrofanov <grelm.wolski@gmail.com>
2025-10-17 15:16:55 +0200

    names form refactor

--- src/components/NamesForm.vue
+++ src/components/NamesForm.vue
@@ -14,39 +14,44 @@ const handleSubmit = async () => {
 
 <template>
   <form class="form-container" @submit.prevent="handleSubmit">
+    <div class="flashing-square"></div>
     <div class="heart"></div>
     <div class="title-container">
-      <div class="title handjet-extra-bold">"LOVE CONTRACT 2025"</div>
-      <div class="title-shadow handjet-extra-bold">"LOVE CONTRACT 2025"</div>
+      <div class="title caveat-bold">"LOVE CONTRACT 2025"</div>
+      <div class="title-shadow caveat-bold">"LOVE CONTRACT 2025"</div>
     </div>
 
     <div class="name-inputs">
+      <div class="title-placeholder handjet-normal">Введите ваши имена</div>
+
       <div>
-        <lable for="partnerOne" class="flex handjet-normal text-white text-[28px] mb-[16px]"
-          >Имя партнера 1</lable
-        >
-        <input
-          id="partnerOne"
-          v-model="partnerOne"
-          type="text"
-          class="handjet-normal text-[28px]"
-          autocomplete="off"
-        />
+        <lable for="partnerOne" class="input-label handjet-normal">Ваше имя</lable>
+        <div class="input-wrapper">
+          <input
+            id="partnerOne"
+            v-model="partnerOne"
+            type="text"
+            class="caveat-bold"
+            autocomplete="off"
+            placeholder="Имя"
+          />
+        </div>
       </div>
       <div class="flex w-full justify-center items-center mt-[8px]">
         <img src="@/assets/plus.svg" class="flex w-[32px] text-center" />
       </div>
       <div>
-        <lable for="partnerTwo" class="flex handjet-normal text-white text-[28px] mb-[16px]"
-          >Имя партнера 2</lable
-        >
-        <input
-          id="partnerTwo"
-          v-model="partnerTwo"
-          type="text"
-          class="handjet-normal text-[28px]"
-          autocomplete="off"
-        />
+        <lable for="partnerTwo" class="input-label handjet-normal">Имя партнера</lable>
+        <div class="input-wrapper">
+          <input
+            id="partnerTwo"
+            v-model="partnerTwo"
+            type="text"
+            class="caveat-bold"
+            autocomplete="off"
+            placeholder="Имя"
+          />
+        </div>
       </div>
     </div>
     <button type="submit" class="name-submit russo-one-regular">
@@ -61,9 +66,21 @@ const handleSubmit = async () => {
   @apply flex flex-col gap-y-[0px] w-[460px] items-center min-h-[512px] mt-[311px] mb-[100px]
   pt-[38px] pb-[80px];
   position: relative;
-  background: linear-gradient(to bottom, #0b002d, #0b0014);
+  background: linear-gradient(to bottom, #0b002d, #170325);
   /* background: gray; */
-  box-shadow: 0 0 4px 0 #00000040;
+  box-shadow: 0 4px 4px 0 #00000040;
+}
+
+.flashing-square {
+  position: absolute;
+  background-image: url('@/assets/flashing-square.svg');
+  background-size: contain;
+  background-repeat: no-repeat;
+  width: 209px;
+  height: 204px;
+  top: -110px;
+  right: -125px;
+  z-index: -1;
 }
 
 .title-container {
@@ -84,7 +101,7 @@ const handleSubmit = async () => {
   background-repeat: no-repeat;
   height: 272.6px;
   width: 296.72px;
-  margin-top: -136.3px;
+  margin-top: -170px;
   top: 50%;
   z-index: 0;
 }
@@ -100,52 +117,106 @@ const handleSubmit = async () => {
 .title-shadow {
   content: '"LOVE CONTRACT 2025"';
   position: absolute;
-  top: 31%;
-  left: 20%;
+  top: 29%;
+  left: 13%;
   background: -webkit-linear-gradient(right, #ddff0050, #dd00ff50);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
   z-index: 0;
 }
 
-.name-inputs {
-  @apply flex flex-col gap-y-[24px] mt-[18px] w-full;
-  padding: 0 32px;
-  z-index: 1;
-}
-
-input {
-  border-radius: 9999px;
-  border: 5px solid #06267f;
-  height: 68px;
-  width: 100%;
-  padding: 0 32px;
-
-  &:focus,
-  &:hover {
-    border: 5px solid #5415f6;
-  }
-}
-
-.name-submit {
-  @apply mt-[80px];
-  position: relative;
+.title-placeholder {
   display: flex;
   width: 100%;
   align-items: center;
   justify-content: center;
+  text-shadow: 2px 2px #E5E82F33;
+  color: #e5e82f;
+  font-size: 32px;
+}
+
+.name-inputs {
+  @apply flex flex-col gap-y-[24px] mt-[0px] w-full;
+  padding: 0 40px;
+  z-index: 1;
+}
+
+.input-label {
+  @apply mb-[16px] pb-[2px];
+  display: flex;
+  color: #d32fe8;
+  font-size: 32px;
+  text-shadow: 3px 1px #ff00f233;
+  text-shadow: 0 0 8px 0 #4e027a80;
+}
+
+.input-wrapper {
+  position: relative;
+  display: inline-block;
+  border-radius: 9999px;
+  width: 100%;
+
+  input {
+    position: relative;
+    border-radius: 9999px;
+    border: 5px solid #802b86;
+    height: 68px;
+    width: 100%;
+    padding: 0 32px;
+    background: none;
+    /* url('@/assets/noise.svg'); */
+    /* mix-blend-mode: multiply; */
+    color: #ffffff;
+    font-size: 28px;
+
+    &::placeholder {
+      color: #9fa7b2;
+    }
+
+    &:focus,
+    &:hover {
+      border: 5px solid #b215f6;
+      box-shadow: 0 0 12px 0 #5415f680;
+    }
+  }
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 9999px;
+    background: url('@/assets/noise.svg');
+    pointer-events: none;
+    mix-blend-mode: multiply;
+    z-index: 5;
+    opacity: 0.1;
+  }
+}
+
+.name-submit {
+  @apply mt-[100px];
+  position: relative;
+  display: flex;
+  width: calc(100% - 80px);
+  align-items: center;
+  justify-content: center;
   gap: 0 10px;
   border-radius: 12px;
-  background: #4627a9;
-  width: 276px;
+  background-image: linear-gradient(to right, #802B86, #75122F);
+  box-shadow: 0 0 12px 0 #802B8699;
   height: 61px;
   color: white;
   font-size: 24px;
-  transition: all 0.3s ease;
+  transition: all 1s ease;
 
   &:hover {
-    background: #5f40c299;
-    box-shadow: 0 0 12px 0 #5f40c299;
+    background-image: linear-gradient(to right, #913198, #861637);
+    box-shadow: 0 0 12px 0 #802B86CC;
   }
 }
 
