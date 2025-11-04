eaccaf9063e1e22c497bac32e018d81320973541
Grigory Mitrofanov <grelm.wolski@gmail.com>
2025-11-04 15:49:20 +0300

    host changed

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 import router from "@/router";
-const baseURL = import.meta.env.VITE_API_URL || "http://localhost:3001";
 
 const api = axios.create({
-	baseURL: import.meta.env.VITE_API_URL ,
+	// baseURL: "http://127.0.0.1:8080",
+	baseURL: "http://80.93.62.150:8080",
 });
 
 api.interceptors.response.use(
