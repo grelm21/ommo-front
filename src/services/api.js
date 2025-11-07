2284df2ff6a1420b4400c917149032cde3859893
Grigory Mitrofanov <grelm.wolski@gmail.com>
2025-11-07 17:18:24 +0300

    api host fix

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import router from "@/router";
 
 const api = axios.create({
-	baseURL: "http://localhost:3001",
+	baseURL: "http://localhost:4040",
 	// baseURL: "http://80.93.62.150:8080",
 });
 
