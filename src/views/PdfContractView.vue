5b01782325b910a540d1298344ecb6945e999862
Grigory Mitrofanov <grelm.wolski@gmail.com>
2025-11-10 21:48:03 +0300

    added placeholders to contract render

--- src/views/PdfContractView.vue
+++ src/views/PdfContractView.vue
@@ -1,77 +1,124 @@
 <template>
   <div class="w-full overflow-auto">
-  <v-pdf
-    ref="pdf"
-    :options="pdfOptions"
-    filename="контракт_любви_оммо.pdf"
-    class="w-[210mm] h-[296mm] relative bg-white mx-auto"
-  >
-    <div class="contract-bg flex flex-col py-2">
-      <p class="playfair-extrabold text-[38px] text-center">Контракт на страхование любви</p>
-      <p class="flex w-1/2 mx-auto playfair-normal text-[20px] text-center mt-2">
-        (Официальный договор о превентивной защите от эмоциональных рисков)
-      </p>
-      <p class="flex w-fit mx-auto playfair-bold mt-2 text-[20px]">ПРЕАМБУЛА:</p>
-      <p class="flex w-full px-[40px] playfair-normal text-[20px] text-center mt-[1px] text-wrap">
-        Стороны,руководствуясь парадоксальным желанием обезопасить себя от хаоса чувств, заключили
-        настоящий договор о нижеследующем. Настоящим подтверждается,что любовь, хоть и является
-        формой высшего безумия, может быть подвержена разумному регулированию.
-      </p>
-      <p class="flex w-fit mx-auto playfair-bold mt-2 text-[20px]">ВЗАИМНЫЕ ОБЯЗАТЕЛЬСТВА</p>
-      <p class="flex w-full px-[40px] playfair-normal text-[20px] text-center mt-[1px] text-wrap">
-        Во исполнение целей настоящего договора Стороны принимают на себя следующие добровольные
-        обязательства:
-      </p>
-
-      <!-- #данные -->
-      <div class="flex flex-grow flex-col items-center justify-center">
-        <div class="flex w-fit mx-auto playfair-bold text-[22px]">{{ nameOne.toUpperCase() }}</div>
-        <p class="flex w-fit px-[40px] playfair-italic text-[20px] text-center mt-[1px] text-wrap">
-          Обязуюсь {{ promiseOne || '' }}
+    <v-pdf
+      ref="pdf"
+      :options="pdfOptions"
+      filename="контракт_любви_оммо.pdf"
+      class="w-[210mm] h-[296mm] relative bg-white mx-auto"
+    >
+      <div class="contract-bg flex flex-col py-2">
+        <p class="playfair-extrabold text-[38px] text-center">Контракт на страхование любви</p>
+        <p class="flex w-1/2 mx-auto playfair-normal text-[20px] text-center mt-2">
+          (Официальный договор о превентивной защите от эмоциональных рисков)
         </p>
-        <div class="flex w-fit mx-auto playfair-bold text-[22px] mt-[30px]">{{ nameTwo.toUpperCase() }}</div>
-        <p class="flex w-fit px-[40px] playfair-italic text-[20px] text-center mt-[1px] text-wrap ">
-          Обязуюсь {{ promiseTwo || '' }}
+        <p class="flex w-fit mx-auto playfair-bold mt-2 text-[20px]">ПРЕАМБУЛА:</p>
+        <p class="flex w-full px-[40px] playfair-normal text-[20px] text-center mt-[1px] text-wrap">
+          Стороны,руководствуясь парадоксальным желанием обезопасить себя от хаоса чувств, заключили
+          настоящий договор о нижеследующем. Настоящим подтверждается,что любовь, хоть и является
+          формой высшего безумия, может быть подвержена разумному регулированию.
         </p>
-      </div>
+        <p class="flex w-fit mx-auto playfair-bold mt-2 text-[20px]">ВЗАИМНЫЕ ОБЯЗАТЕЛЬСТВА</p>
+        <p class="flex w-full px-[40px] playfair-normal text-[20px] text-center mt-[1px] text-wrap">
+          Во исполнение целей настоящего договора Стороны принимают на себя следующие добровольные
+          обязательства:
+        </p>
 
-      <p class="flex w-fit mx-auto playfair-bold mt-2 text-[20px]">ВСТУПЛЕНИЕ В СИЛУ</p>
-      <p class="flex w-full px-[40px] playfair-normal text-[20px] text-center mt-[1px] text-wrap">
-        Настоящий договор вступает в силу 28 ноября,после того как Стороны прослушают песню
-        "страховка любви", предоставленную Агентом. Для активации Полиса необходимо нажать кнопку
-        «пресейв».
-      </p>
-      <p class="flex w-full justify-center playfair-normal text-[20px] mt-2">
-        Поздравляем! Вы составили контракт любви.
-      </p>
-      <p class="flex w-full justify-start playfair-normal text-[16px] mt-2 ms-[40px]">
-        Подписи сторон:
-      </p>
-      <div class="grid grid-cols-2 gap-x-[40px] mt-2 playfair-normal text-[20px] mx-[40px]">
-        <div>Сторона А: _ _ _ _ _ _ _ _ _ _ _ _ _</div>
-        <div>Сторона Б: _ _ _ _ _ _ _ _ _ _ _ _ _</div>
-        <div class="ms-[110px] text-[16px]">(Ваше имя)</div>
-        <div class="flex text-wrap text-[16px]">
-          (Имя второй половинки: человека, кота. R2D2 и других существ обладающих душой)
+        <!-- #данные -->
+        <Transition>        <div class="flex flex-grow flex-col items-center justify-center" v-show="!contractStore.isLoading">
+          <div class="flex w-fit mx-auto playfair-bold text-[22px]">
+            {{ nameOne.toUpperCase() }}
+          </div>
+          <p
+            class="flex w-fit px-[40px] playfair-italic text-[20px] text-center mt-[1px] text-wrap"
+          >
+            Обязуюсь {{ promiseOne || '' }}
+          </p>
+
+          <div class="flex w-fit mx-auto playfair-bold text-[22px] mt-[30px]">
+            {{ nameTwo.toUpperCase() }}
+          </div>
+          <p
+            class="flex w-fit px-[40px] playfair-italic text-[20px] text-center mt-[1px] text-wrap"
+          >
+            Обязуюсь {{ promiseTwo || '' }}
+          </p>
         </div>
-        <div>Подпись и дата: _ _ _ _ _ _ _ _ _ _</div>
-        <div>Подпись и дата: _ _ _ _ _ _ _ _ _ _</div>
+          </Transition>
+<Transition>
+        <div class="flex flex-grow flex-col items-center justify-center" v-show="contractStore.isLoading">
+          <div role="status" class="space-y-3 animate-pulse w-fit">
+            <div class="flex items-center w-full justify-center">
+              <div class="h-[20px] bg-[#9D9D9D] rounded-full w-1/2"></div>
+            </div>
+            <div class="flex items-center justify-center">
+              <div class="h-[20px] bg-[#9D9D9D] rounded-full w-full"></div>
+              <div class="h-[20px] ms-2 bg-[#9D9D9D] rounded-full w-full"></div>
+              <div class="h-[20px] ms-2 bg-[#9D9D9D] rounded-full w-24"></div>
+            </div>
+            <div class="flex items-center justify-center">
+              <div class="h-[20px] bg-[#9D9D9D] rounded-full w-full"></div>
+              <div class="h-[20px] ms-2 bg-[#9D9D9D] rounded-full w-80"></div>
+              <div class="h-[20px] ms-2 bg-[#9D9D9D] rounded-full w-full"></div>
+            </div>
+            <span class="sr-only">Loading...</span>
+          </div>
+
+          <div role="status" class="space-y-3 animate-pulse w-fit mt-[30px]">
+            <div class="flex items-center w-full justify-center">
+              <div class="h-[20px] bg-[#9D9D9D] rounded-full w-1/2"></div>
+            </div>
+            <div class="flex items-center justify-center">
+              <div class="h-[20px] bg-[#9D9D9D] rounded-full w-full"></div>
+              <div class="h-[20px] ms-2 bg-[#9D9D9D] rounded-full w-full"></div>
+              <div class="h-[20px] ms-2 bg-[#9D9D9D] rounded-full w-24"></div>
+            </div>
+            <div class="flex items-center justify-center">
+              <div class="h-[20px] bg-[#9D9D9D] rounded-full w-full"></div>
+              <div class="h-[20px] ms-2 bg-[#9D9D9D] rounded-full w-80"></div>
+              <div class="h-[20px] ms-2 bg-[#9D9D9D] rounded-full w-full"></div>
+            </div>
+            <span class="sr-only">Loading...</span>
+          </div>
+        </div>
+          </Transition>
+
+        <p class="flex w-fit mx-auto playfair-bold mt-2 text-[20px]">ВСТУПЛЕНИЕ В СИЛУ</p>
+        <p class="flex w-full px-[40px] playfair-normal text-[20px] text-center mt-[1px] text-wrap">
+          Настоящий договор вступает в силу 28 ноября,после того как Стороны прослушают песню
+          "страховка любви", предоставленную Агентом. Для активации Полиса необходимо нажать кнопку
+          «пресейв».
+        </p>
+        <p class="flex w-full justify-center playfair-normal text-[20px] mt-2">
+          Поздравляем! Вы составили контракт любви.
+        </p>
+        <p class="flex w-full justify-start playfair-normal text-[16px] mt-2 ms-[40px]">
+          Подписи сторон:
+        </p>
+        <div class="grid grid-cols-2 gap-x-[40px] mt-2 playfair-normal text-[20px] mx-[40px]">
+          <div>Сторона А: _ _ _ _ _ _ _ _ _ _ _ _ _</div>
+          <div>Сторона Б: _ _ _ _ _ _ _ _ _ _ _ _ _</div>
+          <div class="ms-[110px] text-[16px]">(Ваше имя)</div>
+          <div class="flex text-wrap text-[16px]">
+            (Имя второй половинки: человека, кота. R2D2 и других существ обладающих душой)
+          </div>
+          <div>Подпись и дата: _ _ _ _ _ _ _ _ _ _</div>
+          <div>Подпись и дата: _ _ _ _ _ _ _ _ _ _</div>
+        </div>
+        <p class="flex w-full justify-center playfair-normal text-[20px] mt-2">
+          Агент: Музыкальная группа «ОММО»
+        </p>
+        <p class="flex w-full justify-center playfair-normal text-[20px] mt-2">
+          Срок действия: До востребования / До следующего распада Вселенной.
+        </p>
+        <p class="flex w-full justify-start playfair-normal text-[16px] mt-5 ms-[40px]">
+          Служба поддержки страховых полисов любви ОММО: @OMMO_SPB
+        </p>
+        <p class="flex w-full justify-start playfair-normal text-[16px] mt-1 ms-[40px] mb-[20px]">
+          Хештег для предъявления претензий: #СтраховкаЛюбвиОММО
+        </p>
       </div>
-      <p class="flex w-full justify-center playfair-normal text-[20px] mt-2">
-        Агент: Музыкальная группа «ОММО»
-      </p>
-      <p class="flex w-full justify-center playfair-normal text-[20px] mt-2">
-        Срок действия: До востребования / До следующего распада Вселенной.
-      </p>
-      <p class="flex w-full justify-start playfair-normal text-[16px] mt-5 ms-[40px]">
-        Служба поддержки страховых полисов любви ОММО: @OMMO_SPB
-      </p>
-      <p class="flex w-full justify-start playfair-normal text-[16px] mt-1 ms-[40px] mb-[20px]">
-        Хештег для предъявления претензий: #СтраховкаЛюбвиОММО
-      </p>
-    </div>
-  </v-pdf>
-    </div>
+    </v-pdf>
+  </div>
 </template>
 
 <script setup>
@@ -108,14 +155,9 @@ watch(
       nameTwo.value = newContract.partners[1]?.name
       promiseOne.value = newContract.partners[0]?.promise?.description
       promiseTwo.value = newContract.partners[1]?.promise?.description
-      setTimeout(() => {
-        pdf.value.download()
-        // window.close()
-      }, 1000)
     }
   },
 )
-
 </script>
 
 <style scoped>
